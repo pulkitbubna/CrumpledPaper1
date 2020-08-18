df7c6c2adcfb3f7913657b38e31af3344e75ac68
pulkitbubna <68770028+pulkitbubna@users.noreply.github.com>
2020-08-18 19:52:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,40 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body
+const Body = Matter.Body;
 const Render = Matter.Render;
 
-var dusbin, ground, paper;
+var dustbino, papero, groundo;
 var world;
+
 function preload()
 {
 	
 }
 
 function setup() {
-var canvas =	createCanvas(800, 800);
+	createCanvas(1600, 700);
 rectMode(CENTER);
 
 	engine = Engine.create();
 	world = engine.world;
+	dustbino = new dustbin(1200,650);
+	papero = new paper(200,450,40);
+	groundo =  new ground(width/2,670,width,20);
+
+	var render = Render.create({
+		element : document.body,
+		engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false}
+	});
 
 	//Create the Bodies Here.
-    var dusbin = new dusbin(650,650,100,100)
-    ground = new Ground(400,700,800,30);
-    paper = new Paper(100,600,10);
-    var Render = Render.create({ 
-      element: document.body,
-      engine: engine, 
-       options: { 
-       width: 1200,
-       height: 700,
-       wireframes: false
-           } 
-          });
+
+
 	Engine.run(engine);
   
 }
@@ -38,19 +42,20 @@ rectMode(CENTER);
 
 function draw() {
   rectMode(CENTER);
-
   background(0);
-  
-  dusbin.display();
-  ground.display();
-  paper.display();
-  
+  papero.display();
+  groundo.display();
+  dustbino.display();
+  drawSprites();
+ 
 }
 
-function keyDown(){
-  if (keyCode === UP_ARROW) {
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
- Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
-  }
+	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+  
+	}
 }
+
 
